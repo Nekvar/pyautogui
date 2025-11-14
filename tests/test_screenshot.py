0e0f14694bd5cc44b8be12fd95635613a8b1f84b
Nekvar <hriakov91@mail.ru>
2025-11-14 16:30:11 +0300

    change dev screenshot

--- tests/test_screenshot.py
+++ tests/test_screenshot.py
@@ -1,30 +1,7 @@
 import pyautogui
-import os
 import time
 import subprocess
-import pytest
 from datetime import datetime
-import sys
-
-# class TestMyApp:
-#    @pytest.fixture
-#    def app(self):
-#        if os.name == 'nt':
-#            process = subprocess.Popen([
-#                r'Z:\APM-964 Регрессия мастера AirViewSetup_8.4.2.12.02c05c4\AirViewSetup_8.4.2.12.02c05c4\AirView.exe'])
-#
-#        else:
-#            print('False')
-#
-#        time.sleep(3)
-#        yield process
-#        process.terminate()
-#
-#    def test_app_opens_correctly(self, app):
-#        '''Проверка что главное окно открывается'''
-#        window_title = "AirView 8.4.2.12.02c05c4"
-#        window_title = pyautogui.locateOnWindow('images/main_window.png', window_title)
-#        assert window_title is not None, 'Главное окно не найдено'
 
 
 def test_complete_screenshot():
@@ -32,10 +9,8 @@ def test_complete_screenshot():
         [r'Z:\APM-964 Регрессия мастера AirViewSetup_8.4.2.12.02c05c4\AirViewSetup_8.4.2.12.02c05c4\AirView.exe'])
     time.sleep(3)
 
-
     screenshot = pyautogui.screenshot()
     timestamp = datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
     screenshot.save(f'../images/windows/{timestamp}.png')
 
     process.terminate()
-
